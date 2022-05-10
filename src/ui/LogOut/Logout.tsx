5dfc4f3fc7e4ffb55205d74a096791f973a85ce0
Alex <alexandepp@gmail.com>
2022-05-10 16:58:11 +0300

    login css

--- src/ui/LogOut/Logout.tsx
+++ src/ui/LogOut/Logout.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import style from './Logout.module.css'
+import {ActionType, InitStateType} from "../LogIn/LogInTypes";
+import {initState} from "../../bll/logInReducer";
 
 const Logout = () => {
     return (
@@ -13,3 +15,18 @@ const Logout = () => {
 }
 
 export default Logout
+
+export const logInReducer = (state: InitStateType = initState, action: ActionType): InitStateType => {
+    switch (action.type) {
+        case 'APP/SET-LOGIN-IN' :
+            return {...state, isLoginIn: action.isLoggedIn}
+        case 'APP/SET-INITIALIZED':
+            return {...state, isInitialized: action.isInitialized}
+        case 'LOGIN/SET-USER-PROFILE':
+            return {...state, profile: action.profile}
+        case 'LOGIN/FETCH-LOG-ERROR':
+            return {...state, error: action.error}
+        default:
+            return state;
+    }
+}
