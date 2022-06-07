5e23bc043d6f5d40f1a5f02bc7d516c2bcc04c70
Alex <alexandepp@gmail.com>
2022-06-07 20:01:03 +0300

    idea to ignore

--- src/ui/LogIn/LogInTypes.tsx
+++ src/ui/LogIn/LogInTypes.tsx
@@ -4,21 +4,21 @@ import {fetchLogErrorAC, setInitializedAC, setIsLoginAC, setProfileAC} from "./L
 
 export type InitStateType = typeof initState
 
-export type LoginResponseType = {
-    _id: number,
-    email: string,
-    name: string,
-    avatar?: string,
-    publicCardPacksCount: number,
-    created: string,
-    updated: string,
-    isAdmin: boolean,
-    verified: boolean,
-    rememberMe: boolean,
-    token: string,
-    error?: string,
-    access_token?: string
-}
+// export type LoginResponseType = {
+//     _id: number,
+//     email: string,
+//     name: string,
+//     avatar?: string,
+//     publicCardPacksCount: number,
+//     created: string,
+//     updated: string,
+//     isAdmin: boolean,
+//     verified: boolean,
+//     rememberMe: boolean,
+//     token: string,
+//     error?: string,
+//     access_token?: string
+// }
 
 export type LoginType = {
     email: string,
